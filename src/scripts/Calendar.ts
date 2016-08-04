7378d460bb8708a74b5f84112f1827a28dd4ced0
tom-foley <foley.236@osu.edu>
2016-08-04 00:16:00 -0400

    Fixed bugs in changing calendar days; Added more constructor options; Added all click events;

--- src/scripts/Calendar.ts
+++ src/scripts/Calendar.ts
@@ -1,57 +1,79 @@
-import { HTMLElementBuilder, HTMLEventAttr, HTMLStyleAttr, isDefined } from './HTMLElementBuilder';
-
-const style = HTMLStyleAttr;
-const event = HTMLEventAttr;
-
-class Month {
-    index: number;
-    shortName: string;
-    longName: string;
-
-    constructor(index: number, shortName: string, longName: string) {
-        this.index = index;
-        this.shortName = shortName;
-        this.longName = longName;
-    }
+function isDefined(obj) {
+    return typeof (obj) !== 'undefined' && obj !== null;
 }
 
 export class Calendar {
-    selectedDate: Date;
+    //  Elements
+    container: HTMLElement;
+    calendar: HTMLTableElement;
+    selectedCell: HTMLTableCellElement;
 
+    //  Fields
     selectedDay: number;
     selectedMonth: number;
     selectedYear: number;
+    selectedDate: Date;
+    oldSelectedDate: Date;
+    calendarCssClass: string = 'ts-calendar';
+    calendayDayCssClass: string = 'ts-calendar-day';
+    calendarDayActiveCssClass: string = 'ts-calendar-day-active';
 
-    container: HTMLElement;
-    builder: any;
-    months: Array<Month> = [
-        new Month(0, 'Jan', 'January'), new Month(1, 'Feb', 'February'), new Month(2, 'Mar', 'March'),
-        new Month(3, 'Apr', 'April'), new Month(4, 'May', 'May'), new Month(5, 'Jun', 'June'),
-        new Month(6, 'Jul', 'July'), new Month(7, 'Aug', 'August'), new Month(8, 'Sep', 'September'),
-        new Month(9, 'Oct', 'October'), new Month(10, 'Nov', 'November'), new Month(11, 'Dec', 'December'),
+    //  Custom Events  
+    onSelectedDateChanged: any;
+
+    months: Array<string> = [
+        'January', 'February', 'March', 'April',
+        'May', 'June', 'July', 'August',
+        'September', 'October', 'November', 'December',
     ];
+
 
     constructor(container: HTMLElement, options?: Object) {
         this.container = container;
-        this.builder = new HTMLElementBuilder();
-
-        if (isDefined(options)) {
-            let attr: string;
-            attr = 'selectedDate';
-            if (isDefined(options[attr])) {
-                this.setDate(options[attr]);
-            } else {
-                this.setDate(new Date(2016, 6, 15));
-            }
-        } else {
-            this.setDate(new Date(2016, 6, 15));
-        }
-
+        this.setFields(options);
         this.render();
     }
 
 
-    private setDate(date: Date | string) {
+    private setFields(attrs: Object) {
+        if (isDefined(attrs)) {
+            let key: string;
+
+            key = 'selectedDate';
+            if (isDefined(attrs[key])) {
+                this.setDate(attrs[key]);
+            } else {
+                this.setDate(new Date(2016, 6, 15));
+            }
+
+            key = 'calendarCssClass';
+            if (isDefined(attrs[key])) {
+                this.calendarCssClass = attrs[key];
+            }
+
+            key = 'calendayDayCssClass';
+            if (isDefined(attrs[key])) {
+                this.calendayDayCssClass = attrs[key];
+            }
+
+            key = 'calendarSelectedDayCssClass';
+            if (isDefined(attrs[key])) {
+                this.calendarDayActiveCssClass = attrs[key];
+            }
+
+            key = 'onSelectedDateChanged';
+            if (isDefined(attrs[key])) {
+                this.onSelectedDateChanged = attrs[key];
+            }
+        } else {
+            this.setDate(new Date());
+        }
+    }
+
+
+    private setDate(date: Date | string): void {
+        this.oldSelectedDate = this.selectedDate;
+
         if (typeof (date) === 'object') {
             this.selectedDate = <Date>date;
         } else if (typeof (date) === 'string') {
@@ -61,26 +83,15 @@ export class Calendar {
         this.selectedDay = this.selectedDate.getUTCDate();
         this.selectedMonth = this.selectedDate.getUTCMonth();
         this.selectedYear = this.selectedDate.getUTCFullYear();
-        console.log('Day:\t' + this.selectedDay + '\tMonth:\t' + this.selectedMonth + '\tYear:\t' + this.selectedYear);
     }
 
 
-    private generateWeekRow(curDate: Date): HTMLTableRowElement {
-        let tr: HTMLTableRowElement = document.createElement('tr');
-        let day = 0;
-        while (day <= 6) {
-            let td: HTMLTableCellElement = document.createElement('td');
-            td.innerText = curDate.getUTCDate().toString();
-            tr.appendChild(td);
-            curDate.setUTCDate(curDate.getUTCDate() + 1);
-            day++;
-        }
-
-        return tr;
+    private copyDate(source: Date): Date {
+        return new Date(source.getUTCFullYear(), source.getUTCMonth(), source.getUTCDate());
     }
 
 
-    private compareDates(date1: Date, date2: Date) {
+    private compareDates(date1: Date, date2: Date): number {
         if (date1.getUTCFullYear() < date2.getUTCFullYear()) {
             return -1;
         } else if (date1.getUTCFullYear() === date2.getUTCFullYear()) {
@@ -103,18 +114,81 @@ export class Calendar {
     }
 
 
-    private render() {
-        let startDate: Date, endDate: Date;
-        let prevMonthLastDay: Date = new Date(this.selectedYear, this.selectedMonth, 0);
-        let curMonthFirstDay: Date = new Date(this.selectedYear, this.selectedMonth, 1);
-        let curMonthLastDay: Date = new Date(curMonthFirstDay.getUTCFullYear(), curMonthFirstDay.getUTCMonth(), 0);
-        let nextMonthFirstDay: Date = new Date(curMonthFirstDay.getUTCFullYear(), curMonthFirstDay.getUTCMonth() + 1, 1);
+    private setNewDay(date, ev): void {
+        let self = ev.target;
+        this.selectedCell.classList.remove(this.calendarDayActiveCssClass);
+        this.selectedCell = self;
+        this.setDate(date);
+        self.classList.add(this.calendarDayActiveCssClass);
+    }
 
+
+    private setNewMonth(date, ev): void {
+        this.setDate(date);
+        this.container.removeChild(this.calendar);
+        this.render();
+    }
+
+
+    private generateHeaderRow(curMonth: Date, prevMonth: Date, nextMonth: Date): HTMLTableRowElement {
+        let tr: HTMLTableRowElement = document.createElement('tr');
+
+        let btnLeft: HTMLTableHeaderCellElement = document.createElement('th');
+        btnLeft.colSpan = 1;
+        btnLeft.innerText = 'L';
+        btnLeft.onclick = this.setNewMonth.bind(this, this.copyDate(prevMonth));
+
+        let btnRight: HTMLTableHeaderCellElement = document.createElement('th');
+        btnRight.colSpan = 1;
+        btnRight.innerText = 'R';
+        btnRight.onclick = this.setNewMonth.bind(this, this.copyDate(nextMonth));
+
+        let title: HTMLTableHeaderCellElement = document.createElement('th');
+        title.colSpan = 5;
+        title.innerText = this.months[curMonth.getUTCMonth()] + ' ' + curMonth.getUTCFullYear();
+
+        tr.appendChild(btnLeft);
+        tr.appendChild(title);
+        tr.appendChild(btnRight);
+        return tr;
+    }
+
+
+    private generateWeekRow(curDate: Date): HTMLTableRowElement {
+        let tr: HTMLTableRowElement = document.createElement('tr');
+
+        for (let i: number = 0; i < 7; ++i) {
+            let td: HTMLTableCellElement = document.createElement('td');
+            td.className = this.calendayDayCssClass;
+            td.innerText = curDate.getUTCDate().toString();
+
+            if (this.compareDates(curDate, this.selectedDate) === 0) {
+                td.classList.add(this.calendarDayActiveCssClass);
+                this.selectedCell = td;
+            } else if (curDate.getUTCMonth() < this.selectedMonth || curDate.getUTCMonth() > this.selectedMonth) {
+                td.onclick = this.setNewMonth.bind(this, this.copyDate(curDate));
+            } else {
+                td.onclick = this.setNewDay.bind(this, this.copyDate(curDate));
+            }
+
+            tr.appendChild(td);
+            curDate.setUTCDate(curDate.getUTCDate() + 1);
+        }
+
+        return tr;
+    }
+
+
+    private render() {
+        const prevMonthLastDay: Date = new Date(this.selectedYear, this.selectedMonth, 0);
+        const curMonthFirstDay: Date = new Date(this.selectedYear, this.selectedMonth, 1);
+        const curMonthLastDay: Date = new Date(curMonthFirstDay.getUTCFullYear(), curMonthFirstDay.getUTCMonth() + 1, 0);
+        const nextMonthFirstDay: Date = new Date(curMonthFirstDay.getUTCFullYear(), curMonthFirstDay.getUTCMonth() + 1, 1);
+
+        let startDate: Date, endDate: Date;
         if (curMonthFirstDay.getUTCDay() === 0) {
-            console.log('Start Date = 0');
             startDate = curMonthFirstDay;
         } else {
-            console.log('Start Date > 0');
             startDate = new Date(
                 prevMonthLastDay.getUTCFullYear(),
                 prevMonthLastDay.getUTCMonth(),
@@ -122,11 +196,9 @@ export class Calendar {
             );
         }
 
-        if (curMonthLastDay.getUTCDay() === 6) {
-            console.log('End Date = 6');
+        if (curMonthLastDay.getUTCDay() === 0) {
             endDate = curMonthLastDay;
         } else {
-            console.log('End Date < 6');
             endDate = new Date(
                 nextMonthFirstDay.getUTCFullYear(),
                 nextMonthFirstDay.getUTCMonth(),
@@ -134,20 +206,22 @@ export class Calendar {
             );
         }
 
-        console.log(startDate);
-        console.log(endDate);
+        let table: HTMLTableElement = document.createElement('table');
+        table.className = this.calendarCssClass;
 
-        let table: HTMLElement = this.builder.createEl({
-            type: 'table',
-            styles: [
-                new style('width', '100%')
-            ]
-        });
+        let thead: HTMLTableSectionElement = document.createElement('thead');
+        thead.appendChild(
+            this.generateHeaderRow(curMonthFirstDay, prevMonthLastDay, nextMonthFirstDay)
+        );
 
+        let tbody: HTMLTableSectionElement = document.createElement('tbody');
         while (this.compareDates(startDate, endDate) < 1) {
-            table.appendChild(this.generateWeekRow(startDate));
+            tbody.appendChild(this.generateWeekRow(startDate));
         }
 
+        table.appendChild(thead);
+        table.appendChild(tbody);
+        this.calendar = table;
         this.container.appendChild(table);
     }
 }
